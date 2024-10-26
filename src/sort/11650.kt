9d1413b38414b9d848405c60d1ceab908a1092ce
stdiodh <playlistdh@gmail.com>
2024-10-26 12:04:36 +0900

    11650

---
+++ src/sort/11650.kt
@@ -0,0 +1,48 @@
+package sort
+
+//import java.io.StreamTokenizer
+
+/*
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+    fun nextInt(): Int {
+        nextToken()
+        return nval.toInt()
+    }
+
+    val n = nextInt()
+    val arrX = Array(n) { 0 }
+    val arrY = Array(n) { 0 }
+
+    repeat(n) { idx ->
+        arrX[idx] = nextInt()
+        arrY[idx] = nextInt()
+    }
+
+    val XY = arrX.zip(arrY)
+    val result = XY.sortedWith(compareBy({ it.first }, { it.second }))
+
+    repeat(n){
+        println("${result[it].first} ${result[it].second}")
+    }
+}
+*/
+
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+    fun nextInt(): Int {
+        nextToken()
+        return nval.toInt()
+    }
+
+    val n = nextInt()
+    val pairs = Array(n) {Pair(0, 0)}
+    repeat(n){ i->
+        pairs[i] = Pair(nextInt(), nextInt())
+    }
+    pairs.sortWith(compareBy<Pair<Int, Int>> { it.first }.thenBy { it.second } )
+
+    repeat(n){
+        println("${pairs[it].first} ${pairs[it].second}")
+    }
+}
