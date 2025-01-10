78abdbc8db342817a4740c7367270e6c60842639
stdiodh <playlistdh@gmail.com>
2025-01-10 11:45:00 +0900

    2156

--- src/dp/2156.kt
+++ src/dp/2156.kt
@@ -7,21 +7,28 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
     fun nextInt() : Int { nextToken(); return nval.toInt() }
 
     val n = nextInt()
-    val arr = IntArray(n) { nextInt() }
-    val dp = IntArray(n) { 0 }
+    val grape = IntArray(n+2) { 0 }
+    val dp = IntArray(n+2) { 0 }
 
-    dp[0] = arr[0]
+    // 헷갈리지 않기 위해 인덱스 1부터 시작
+    grape[0] = 0
 
-    if(n > 1){
-        dp[1] = arr[0] + arr[1]
-    }
-    if(n > 2){
-        dp[2] = max(arr[0] + arr[2], arr[1] + arr[2])
-    }
-
-    for(x in 3..< n){
-        dp[x] = max(dp[x-3] + arr[x-1] + arr[x], dp[x-2] + arr[x])
+    // 포도주 잔 입력
+    for(i in 1 .. n){
+        grape[i] = nextInt()
     }
 
-    print(dp.max())
+    //초기값 설정
+    dp[1] = grape[1]
+    dp[2] = grape[1] + grape[2]
+
+    //점화식
+    for(i in 3 .. n){
+        // 1번째 와인을 마시는 경우 중 최대 값
+        val max = max(dp[i-3] + grape[i-1] + grape[i], dp[i-2] + grape[i])
+        // 1번쨔 와인을 마시는 or 안 마시는 경우 중 최대 값
+        dp[i] = max(dp[i-1], max)
+    }
+
+    print(dp[n])
 }
