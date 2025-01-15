b2785da95566a9a3ee4ebc7facda3a3beb93ddd4
stdiodh <playlistdh@gmail.com>
2025-01-15 12:54:50 +0900

    1343

--- src/greedy/1343.kt
+++ src/greedy/1343.kt
@@ -2,10 +2,37 @@ package greedy
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    val str = br.readLine().replace("XXXX", "AAAA").replace("XX", "BB")
-    if(str.contains('X')){
-        print(-1)
-    } else {
-        print(str)
+    val bw = System.out.bufferedWriter()
+
+    val line = br.readLine()
+    var str = StringBuilder()
+    var count = 0
+    for (i in 0 .. line.count()) {
+        if (i == line.count() || line[i] == '.') {
+            if (count % 2 == 1) {
+                str = StringBuilder()
+                str.append(-1)
+                break
+            }
+
+            while (count - 4 >= 0) {
+                str.append("AAAA")
+                count -= 4
+            }
+            while (count - 2 >= 0) {
+                str.append("BB")
+                count -= 2
+            }
+            if (i < line.count()) {
+                str.append(".")
+            }
+            count = 0
+        } else {
+            count += 1
+        }
     }
+    bw.write(str.toString())
+    br.close()
+    bw.flush()
+    bw.close()
 }
