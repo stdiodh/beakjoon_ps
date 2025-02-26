4b81e26f8a0ae97fb484af1f1e0d2ca6a1df5b7d
stdiodh <playlistdh@gmail.com>
2025-02-26 09:44:02 +0900

    4963

--- src/bfsdfs/4963.kt
+++ src/bfsdfs/4963.kt
@@ -1,34 +1,46 @@
 package bfsdfs
 
 import java.io.StreamTokenizer
-import java.util.StringTokenizer
 
 fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
     fun nextInt() : Int { nextToken(); return nval.toInt() }
     val br = System.`in`.bufferedReader()
+    val dx = intArrayOf(0, 1, 0, -1, -1, 1, -1, 1)
+    val dy = intArrayOf(1, 0, -1, 0, -1, 1, 1, -1)
 
     while(true){
-        val (w, h) = br.readLine().split(" ").map { it.toInt() }
+        val w = nextInt(); val h = nextInt()
         if(w == 0 && h == 0){
             break
         }
-        val graph = Array(h) { Array(w) { 0 } }
+        val graph = Array(h) { Array(w) { nextInt() } }
         val visited = Array(h) { BooleanArray(w) { false } }
 
-        repeat(h) { x ->
-            val st = StringTokenizer(readLine())
-            repeat(w) { y ->
-                graph[x][y] = st.nextToken().toInt()
+        fun dfs(x: Int, y: Int){
+            visited[x][y] = true
+
+            for(i in 0..< 8){
+                val nx = dx[i] + x
+                val ny = dy[i] + y
+
+                if(nx < 0 || nx >= h || ny < 0 || ny >= w){
+                    continue
+                }
+                if(!visited[nx][ny] && graph[nx][ny] == 1){
+                    dfs(nx, ny)
+                }
             }
         }
 
-        graph.forEach { println(it.toList()) }
-
-
-    }
-    val dx = intArrayOf(0, 1, 0, -1)
-    val dy = intArrayOf(1, 0, -1, 0)
-    fun dfs(x: Int, y: Int){
-
+        var ans = 0
+        repeat(h){x ->
+            repeat(w){y ->
+                if(graph[x][y] == 1 && !visited[x][y]){
+                    ans++
+                    dfs(x, y)
+                }
+            }
+        }
+        println(ans)
     }
 }
